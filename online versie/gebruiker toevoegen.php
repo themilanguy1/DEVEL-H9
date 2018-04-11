630a56a820b1c2553f67e4c7bcaa343671cd54b3
themilanguy1 <milan.gupta@ymail.com>
2018-04-11 13:24:55 +0200

    online versie aangemaakt

---
+++ online versie/gebruiker toevoegen.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible">
+    <title>Gebruiker Toevoegen</title>
+</head>
+<body>
+
+    <?php
+    session_start();
+        try {
+            $db = new PDO("mysql:host=localhost;dbname=fietsenmaker", "root", "");
+            if(isset($_POST['inloggen'])) {
+                $username = $_POST['username'];
+                $password = sha1($_POST['password']);
+                $query = $db->prepare("SELECT * FROM gebruiker 
+                    WHERE username = :user 
+                    AND password = :pass");
+                $query->bindParam("user", $username);
+                $query->bindParam("pass", $password);
+                $query->execute();
+                if($query->rowCount() == 1) {
+                    echo "Juiste gegevens!";
+                    $_SESSION['login'] = true;
+                } else {
+                    echo "Onjuiste gegevens!";
+                    $_SESSION['login'] = false;
+                }
+            } echo "<br>";
+        } catch(PDOException $e) {
+            die("Error!: " . $e->getMessage());
+        }
+    ?>
+    <form method="post" action="">
+        <label>Username</label>
+        <input type="text" name="username"><br>
+
+        <label>Password</label>
+        <input type="password" name="password"><br>
+
+        <input type="submit" name="inloggen" value="Inloggen">
+    </form>
+
+
+<?php
+if(isset($_SESSION['login'])) {
+    if($_SESSION['login'] == true) {
+        header('location: beveiligdepagina.php');
+        exit;
+    }
+}
+
+?>
+
+</body>
+</html>
